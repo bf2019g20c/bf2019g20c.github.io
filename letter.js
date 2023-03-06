792e31df953fcd74c68fa3b1869eb5502bbf7d51
2019School20Class <121164932+bf2019g20c@users.noreply.github.com>
2023-03-06 13:16:49 +0800

    Add files via upload

--- letter.js
+++ letter.js
@@ -1,6 +1,7 @@
-// Wrap every letter in a span
 var textWrapper = document.querySelector('.ml11 .letters');
 textWrapper.innerHTML = textWrapper.textContent.replace(/([^\x00-\x80]|\w)/g, "<span class='letter'>$&</span>");
+var textWrapper = document.querySelector('.ml14 .letters');
+textWrapper.innerHTML = textWrapper.textContent.replace(/\S/g, "<span class='letter'>$&</span>");
 
 anime.timeline({loop: true})
   .add({
@@ -25,6 +26,30 @@ anime.timeline({loop: true})
     delay: (el, i) => 34 * (i+1)
   }).add({
     targets: '.ml11',
+    opacity: 0,
+    duration: 1000,
+    easing: "easeOutExpo",
+    delay: 1000
+  });
+anime.timeline({loop: true})
+  .add({
+    targets: '.ml14 .line',
+    scaleX: [0,1],
+    opacity: [0.5,1],
+    easing: "easeInOutExpo",
+    duration: 900
+  }).add({
+    targets: '.ml14 .letter',
+    opacity: [0,1],
+    translateX: [40,0],
+    translateZ: 0,
+    scaleX: [0.3, 1],
+    easing: "easeOutExpo",
+    duration: 800,
+    offset: '-=600',
+    delay: (el, i) => 150 + 25 * i
+  }).add({
+    targets: '.ml14',
     opacity: 0,
     duration: 1000,
     easing: "easeOutExpo",
